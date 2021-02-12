09f767ddfa530e4ef1c0e02ef9fbe97b9a0f70a5
bruce <2432001677@qq.com>
2021-02-12 22:22:18 +0800

    add CORS configuration in gateway

---
+++ qop-gateway/src/main/java/cn/edu/zucc/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package cn.edu.zucc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @author Bruce
+ * @since 02-12-2021
+ * <p>
+ * CORS Configuration
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedOrigin("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", configuration);
+        return new CorsWebFilter(source);
+    }
+}
