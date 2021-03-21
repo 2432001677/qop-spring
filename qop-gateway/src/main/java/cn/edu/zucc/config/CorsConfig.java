23f78f5cd5850f673ea229c5a2e22b235b0db026
bruce <2432001677@qq.com>
2021-03-21 21:44:18 +0800

    update cors config

--- qop-gateway/src/main/java/cn/edu/zucc/config/CorsConfig.java
+++ qop-gateway/src/main/java/cn/edu/zucc/config/CorsConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.util.pattern.PathPatternParser;
 
 /**
  * @author Bruce
@@ -18,12 +17,13 @@ public class CorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedOrigin("*");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
-        configuration.addAllowedOrigin("*");
-        configuration.setAllowCredentials(true);
+        configuration.addExposedHeader("Authorization");
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return new CorsWebFilter(source);
     }
